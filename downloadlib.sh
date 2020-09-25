e47c4da74d03edb629b52463eb58d530b1264dee
elzup <guild0105@gmail.com>
2020-09-25 21:46:20 +0900

    add setup script

---
+++ downloadlib.sh
@@ -0,0 +1,3 @@
+curl https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css > dist/github-markdown.min.css
+curl https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.2.0/styles/atom-one-dark-reasonable.min.css > dist/atom-one-dark-reasonable.min.css
+curl https://cdnjs.cloudflare.com/ajax/libs/highlight.js/10.2.0/highlight.min.js > dist/highlight.min.js
